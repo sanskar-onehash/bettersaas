a847e1bd78316189c79b406767ea19089abcd2e0
Sanskar Soni <sanskarsoni300@gmail.com>
2025-08-08 07:54:28 +0530

    fix(f2b): add/remove ignoreip individually
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- bettersaas/fail2ban/fail2ban.py
+++ bettersaas/fail2ban/fail2ban.py
@@ -14,14 +14,20 @@ def get_root_password():
 def set_ignore_ips(ip_list):
     if not ip_list:
         return
-    ips = " ".join(ip_list)
-    cmd = f"echo {get_root_password()} | sudo -S fail2ban-client set {JAIL_NAME} addignoreip {ips}"
-    subprocess.call(cmd, shell=True)
+    for ip in ip_list:
+        try:
+            cmd = f"echo {get_root_password()} | sudo -S fail2ban-client set {JAIL_NAME} addignoreip {ip}"
+            subprocess.call(cmd, shell=True)
+        except Exception as e:
+            frappe.log_error("Error occured while addingoreip", e)
 
 
 def remove_ignore_ips(ip_list):
     if not ip_list:
         return
-    ips = " ".join(ip_list)
-    cmd = f"echo {get_root_password()} | sudo -S fail2ban-client set {JAIL_NAME} delignoreip {ips}"
-    subprocess.call(cmd, shell=True)
+    for ip in ip_list:
+        try:
+            cmd = f"echo {get_root_password()} | sudo -S fail2ban-client set {JAIL_NAME} delignoreip {ip}"
+            subprocess.call(cmd, shell=True)
+        except Exception as e:
+            frappe.log_error("Error occured while delingoreip", e)
